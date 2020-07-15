89e0a59e785d0c74952b9547fc8910b94fbb9aaa
Craig <craigdanj@gmail.com>
2020-07-16 01:11:04 +0530

    basic auth

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 
 class App extends React.Component {
 
+	state = {
+		showHome: false
+	};
+
 	loginClicked = () => {
 		fetch("http://localhost:8080/sessions/connect") 
 		.then(response => response.json()) 
@@ -11,12 +15,36 @@ class App extends React.Component {
 		}); 
 	};
 
+
+	componentDidMount() {
+		const urlParams = new URLSearchParams(window.location.search);
+		const oauthToken = urlParams.get('oauth_token');
+		const oauthVerifier = urlParams.get('oauth_verifier');
+		console.log(oauthToken, oauthVerifier);
+
+		if (oauthToken && oauthVerifier) {
+			this.setState({
+				showHome: true
+			});
+		}
+	}
+
 	render() {
+
+		const login = (
+			<div>
+				<button className="login" onClick={this.loginClicked}>Login with Twitter</button>
+			</div>
+		);
+
+		const home = (<div>Home</div>)
 
 		return (
 			<div className="App">
 				<h1>The Twitter App</h1>
-				<button className="login" onClick={this.loginClicked}>Login with Twitter</button>
+
+				{ this.state.showHome ? home : login }
+				
 			</div>
 		);
 	}
