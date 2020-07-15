662a94f1fd18d48b0466ce0c52fe0c801503b7f5
Craig <craigdanj@gmail.com>
2020-07-16 01:36:15 +0530

    taken save logic

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 class App extends React.Component {
 
 	state = {
-		showHome: false
+		showHome: false,
+		oauthToken: null,
+		oauthVerifier: null
 	};
 
 	loginClicked = () => {
@@ -15,16 +17,29 @@ class App extends React.Component {
 		}); 
 	};
 
+	saveToken = () => {
+		console.log(this.state.oauthToken, this.state.oauthVerifier);
+
+		fetch(`http://localhost:8080/sessions/saveAccessTokens?oauth_token=${this.state.oauthToken}&oauth_verifier=${this.state.oauthVerifier}`) 
+		.then(response => response.json()) 
+		.then(data => {
+			console.log(data);
+			// window.location.href = data.redirectUrl;
+		}); 
+	};
+
 
 	componentDidMount() {
 		const urlParams = new URLSearchParams(window.location.search);
 		const oauthToken = urlParams.get('oauth_token');
 		const oauthVerifier = urlParams.get('oauth_verifier');
-		console.log(oauthToken, oauthVerifier);
+		// console.log(oauthToken, oauthVerifier);
 
 		if (oauthToken && oauthVerifier) {
 			this.setState({
-				showHome: true
+				showHome: true,
+				oauthToken,
+				oauthVerifier
 			});
 		}
 	}
@@ -37,7 +52,13 @@ class App extends React.Component {
 			</div>
 		);
 
-		const home = (<div>Home</div>)
+		const home = (
+			<div>
+				Home
+				
+				<button onClick={this.saveToken}>Boom</button>
+			</div>
+		)
 
 		return (
 			<div className="App">
