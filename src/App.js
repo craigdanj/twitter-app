76e44f9ffed2d1732c98113eb6b31fd158c00bdc
Craig <craigdanj@gmail.com>
2020-07-16 00:58:44 +0530

    Basic login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+	loginClicked = () => {
+		fetch("http://localhost:8080/sessions/connect") 
+		.then(response => response.json()) 
+		.then(data => {
+			window.location.href = data.redirectUrl;
+		}); 
+	};
+
+	render() {
+
+		return (
+			<div className="App">
+				<h1>The Twitter App</h1>
+				<button className="login" onClick={this.loginClicked}>Login with Twitter</button>
+			</div>
+		);
+	}
 }
 
 export default App;
